545f8b092867743432f1a9a65481f87b2b5eb69a
nishitgangani29@gmail.com <nishitgangani29@gmail.com>
2021-02-07 16:00:52 +0530

    Google Sheet and remove item

--- app/src/main/java/com/example/googlesheet/Adapter.java
+++ app/src/main/java/com/example/googlesheet/Adapter.java
@@ -9,41 +9,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.ads.nativetemplates.TemplateView;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.MyViewholder> {
+public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context context;
-    private List<Sheet1> data;
-
-    public Adapter(Context context, List<Sheet1> data) {
-        this.context = context;
-        this.data = data;
-    }
-
-    @NonNull
-    @Override
-    public Adapter.MyViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        View view = LayoutInflater.from(context).inflate(R.layout.item_text, parent, false);
-
-        return new MyViewholder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull Adapter.MyViewholder holder, int i) {
-
-        Sheet1 user = data.get(i);
-
-        holder.id.setText(user.getName());
-        holder.name.setText(user.getCountry());
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return data.size();
-    }
+    private ArrayList<Object> data;
+    private static final int MENU_ITEM_VIEW_TYPE = 0;
+    private static final int UNIFIED_NATIVE_AD_VIEW_TYPE = 1;
+    private removeItem removeItem;
 
     public class MyViewholder extends RecyclerView.ViewHolder {
 
@@ -58,5 +38,116 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewholder> {
         }
     }
 
+    public Adapter(Context context, ArrayList<Object> data,removeItem removeItem) {
+        this.context = context;
+        this.data = data;
+        this.removeItem = removeItem;
+    }
+
+
+
+
+    public class AdViewHolder extends RecyclerView.ViewHolder {
+
+        private TemplateView myTemplate;
+
+        public AdViewHolder(View view) {
+            super(view);
+
+            myTemplate = view.findViewById(R.id.my_template);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        Object recyclerViewItem = data.get(position);
+        if (recyclerViewItem instanceof UnifiedNativeAd) {
+            return UNIFIED_NATIVE_AD_VIEW_TYPE;
+        }
+        return MENU_ITEM_VIEW_TYPE;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case UNIFIED_NATIVE_AD_VIEW_TYPE:
+                View nativeAdView = LayoutInflater.from(
+                        parent.getContext()).inflate(R.layout.item_native_ad, parent, false);
+
+                return new Adapter.AdViewHolder(nativeAdView);
+            case MENU_ITEM_VIEW_TYPE:
+                // Fall through.
+            default:
+                View view = LayoutInflater.from(context).inflate(R.layout.item_text, parent, false);
+
+                return new MyViewholder(view);
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+
+        int viewType = getItemViewType(position);
+
+        switch (viewType){
+            case UNIFIED_NATIVE_AD_VIEW_TYPE:
+                final AdViewHolder adViewHolder = (AdViewHolder)holder;
+
+                AdLoader.Builder adbuilder = new AdLoader.Builder(context, "ca-app-pub-3940256099942544/2247696110");
+
+                adbuilder.forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
+                    @Override
+                    public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
+                        adViewHolder.myTemplate.setNativeAd(unifiedNativeAd);
+                    }
+                });
+
+                AdLoader adLoader = adbuilder.build();
+                adLoader.loadAd(new AdRequest.Builder().build());
+
+                break;
+            case MENU_ITEM_VIEW_TYPE:
+                // fall through
+            default:
+                MyViewholder myViewHolder = (MyViewholder) holder;
+                final Sheet1 user = (Sheet1) data.get(position);
+                ((MyViewholder) holder).id.setText(user.getName());
+                ((MyViewholder) holder).name.setText(user.getCountry());
+
+                ((MyViewholder) holder).itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View view) {
+                        removeItem.removeItemfromArray(position);
+                        return true;
+                    }
+                });
+        }
+
+
+
+    }
+
+/*    public void onBindViewHolder(@NonNull Adapter.MyViewholder holder, int i) {
+
+        Sheet1 user = data.get(i);
+
+            holder.id.setText(user.getName());
+            holder.name.setText(user.getCountry());
+
+    }*/
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+
+    public interface removeItem{
+
+        void removeItemfromArray(int position);
+    }
 
 }
--- app/src/main/java/com/example/googlesheet/MainActivity.java
+++ app/src/main/java/com/example/googlesheet/MainActivity.java
@@ -15,22 +15,31 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Adapter.removeItem {
 
     private RecyclerView rview;
     private SwipeRefreshLayout swipeRefreshLayout;
 
     private static final String URL = "https://script.google.com/macros/s/AKfycbxOLElujQcy1-ZUer1KgEvK16gkTLUqYftApjNCM_IRTL3HSuDk/exec?id=159--pHYbfd56xjdLpNau0Db58Qz8L2r8kGf-WY8l8Ww&sheet=Sheet1";
-    private List<Sheet1> sheet1List;
+    private ArrayList<Object> sheet1List = new ArrayList<>();
 
     private Adapter adapter;
     private RequestQueue requestQueue;
+
+    private AdLoader adLoader;
+    private List<UnifiedNativeAd> mNativeAds = new ArrayList<>();
+    public static final int NUMBER_OF_ADS = 5;
+    private ModelMain modelMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +49,14 @@ public class MainActivity extends AppCompatActivity {
         rview = findViewById(R.id.rview);
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
 
+        mNativeAds.clear();
 
-        sheet1List = new ArrayList<>();
 
+
+        loadNativeAds();
 
         rview.setHasFixedSize(true);
-        adapter = new Adapter(this,sheet1List);
+        adapter = new Adapter(this,sheet1List,this);
         rview.setLayoutManager(new LinearLayoutManager(this));
         rview.setAdapter(adapter);
         JsonRequest();
@@ -58,6 +69,11 @@ public class MainActivity extends AppCompatActivity {
             public void onRefresh() {
                 adapter.notifyDataSetChanged();
                 swipeRefreshLayout.setRefreshing(false);
+                mNativeAds.clear();
+                onRefreshRecycle();
+
+                loadNativeAds();
+
             }
         });
     }
@@ -73,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
                 GsonBuilder gsonBuilder = new GsonBuilder();
                 Gson gson = gsonBuilder.serializeNulls().create();
 
-                ModelMain modelMain = gson.fromJson(response,ModelMain.class);
+                 modelMain = gson.fromJson(response,ModelMain.class);
 
                 sheet1List.clear();
 
@@ -93,5 +109,81 @@ public class MainActivity extends AppCompatActivity {
 
         requestQueue = Volley.newRequestQueue(MainActivity.this);
         requestQueue.add(stringRequest);
+        loadNativeAds();
+
     }
+
+    private void loadNativeAds() {
+
+        AdLoader.Builder builder = new AdLoader.Builder(this, "ca-app-pub-3940256099942544/2247696110");
+        adLoader = builder.forUnifiedNativeAd(
+                new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
+                    @Override
+                    public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
+                        // A native ad loaded successfully, check if the ad loader has finished loading
+                        // and if so, insert the ads into the list.
+                        mNativeAds.add(unifiedNativeAd);
+                        if (!adLoader.isLoading()) {
+                            insertAdsInMenuItems();
+                        }
+                    }
+                }).withAdListener(
+                new AdListener() {
+                    @Override
+                    public void onAdFailedToLoad(int errorCode) {
+                        // A native ad failed to load, check if the ad loader has finished loading
+                        // and if so, insert the ads into the list.
+                        Log.e("MainActivity", "The previous native ad failed to load. Attempting to"
+                                + " load another.");
+                        if (!adLoader.isLoading()) {
+                            insertAdsInMenuItems();
+                        }
+                    }
+                }).build();
+
+        // Load the Native ads.
+        adLoader.loadAds(new AdRequest.Builder().build(), sheet1List.size());
+    }
+
+    private void insertAdsInMenuItems() {
+        if (mNativeAds.size() <= 0) {
+            return;
+        }
+
+        //int offset = (videoList.size() / mNativeAds.size()) + 1;
+        //int offset = 3;
+        int index = 3;
+        /*for (UnifiedNativeAd ad : mNativeAds) {
+            videoList.add(index, ad);
+            index = index + offset;
+        }*/
+
+        for (int i = 0; i <= mNativeAds.size(); i++) {
+            if (index < sheet1List.size()) {
+                sheet1List.add(index, mNativeAds.get(i));
+                index = index + 4;
+            } else {
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void removeItemfromArray(int position) {
+
+        sheet1List.remove(position);
+        adapter.notifyDataSetChanged();
+
+
+    }
+
+    private void onRefreshRecycle() {
+        sheet1List.clear();
+
+        sheet1List.addAll(modelMain.getSheet1());
+
+        adapter.notifyDataSetChanged();
+    }
+
+
 }
